758a5071d67784732bf7d34e58f3c15757193dc0
Nicolás <nicolascayuman77@gmail.com>
2026-06-26 22:06:46 -0400

    feat(graphics): add visual markers and lines for key conic elements

--- RutGeo.UI/Helpers/ConicPlotService.cs
+++ RutGeo.UI/Helpers/ConicPlotService.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using ScottPlot.Avalonia;
+using ScottPlot;
 using RutGeo.Core.Models;
 using RutGeo.Core.Models.Equations;
 using RutGeo.Core.Models.Types;
+using RutGeo.Core.Helpers;
 
 namespace RutGeo.UI.Helpers;
 
@@ -51,6 +53,8 @@ public class ConicPlotService
         if (xs.Count > 1)
             RenderSegments(xs, ys);
 
+        RenderConicElements(canon?.Elements);
+
         // Si es circunferencia o elipse, AutoScale funciona perfecto.
         // Si es hipérbola o parábola generada masivamente, limitamos la vista inicial 
         // para que no se vea minúscula.
@@ -93,5 +97,115 @@ public class ConicPlotService
 
         if (segXs.Count > 1)
             _plot.AddScatterNoMarkers(segXs.ToArray(), segYs.ToArray());
+    }
+
+    private void RenderConicElements(ConicElements? elements)
+    {
+        if (elements == null) return;
+        
+        var mainColor = Colors.Red;
+        var subColor = Colors.Orange;
+        
+        if (elements.Center != null)
+        {
+            var m = _plot.Plot.Add.Marker(elements.Center.X, elements.Center.Y);
+            m.Color = mainColor;
+            m.Shape = MarkerShape.FilledCircle;
+        }
+
+        if (elements is EllipseElements ellipse)
+        {
+            foreach (var f in ellipse.Foci)
+            {
+                var m = _plot.Plot.Add.Marker(f.X, f.Y);
+                m.Color = subColor;
+                m.Shape = MarkerShape.OpenCircle;
+            }
+            foreach (var v in ellipse.MajorVertices)
+            {
+                var m = _plot.Plot.Add.Marker(v.X, v.Y);
+                m.Color = mainColor;
+                m.Shape = MarkerShape.FilledCircle;
+            }
+            foreach (var v in ellipse.MinorVertices)
+            {
+                var m = _plot.Plot.Add.Marker(v.X, v.Y);
+                m.Color = mainColor;
+                m.Shape = MarkerShape.FilledCircle;
+            }
+        }
+        else if (elements is ParabolaElements parabola)
+        {
+            var m = _plot.Plot.Add.Marker(parabola.Vertex.X, parabola.Vertex.Y);
+            m.Color = mainColor;
+            m.Shape = MarkerShape.FilledCircle;
+
+            var f = _plot.Plot.Add.Marker(parabola.Focus.X, parabola.Focus.Y);
+            f.Color = subColor;
+            f.Shape = MarkerShape.OpenCircle;
+
+            DrawLine2D(parabola.Directrix, Colors.Gray);
+            
+            double halfLr = 2 * RutGeoMath.Abs(parabola.FocalDistance);
+            if (RutGeoMath.Abs(parabola.AxisOfSymmetry.A) > 0.5)
+            {
+                var lr = _plot.Plot.Add.Line(parabola.Focus.X - halfLr, parabola.Focus.Y, parabola.Focus.X + halfLr, parabola.Focus.Y);
+                lr.Color = Colors.Blue;
+                lr.LinePattern = LinePattern.Dashed;
+            }
+            else
+            {
+                var lr = _plot.Plot.Add.Line(parabola.Focus.X, parabola.Focus.Y - halfLr, parabola.Focus.X, parabola.Focus.Y + halfLr);
+                lr.Color = Colors.Blue;
+                lr.LinePattern = LinePattern.Dashed;
+            }
+        }
+        else if (elements is HyperbolaElements hyperbola)
+        {
+            foreach (var f in hyperbola.Foci)
+            {
+                var m = _plot.Plot.Add.Marker(f.X, f.Y);
+                m.Color = subColor;
+                m.Shape = MarkerShape.OpenCircle;
+            }
+            foreach (var v in hyperbola.Vertices)
+            {
+                var m = _plot.Plot.Add.Marker(v.X, v.Y);
+                m.Color = mainColor;
+                m.Shape = MarkerShape.FilledCircle;
+            }
+            foreach (var asympt in hyperbola.Asymptotes)
+            {
+                DrawLine2D(asympt, Colors.Gray);
+            }
+        }
+    }
+
+    private void DrawLine2D(Line2D line, Color color)
+    {
+        
+        if (RutGeoMath.Abs(line.B) < 0.0001)
+        {
+            
+            if (RutGeoMath.Abs(line.A) > 0.0001)
+            {
+                double x = -line.C / line.A;
+                var vl = _plot.Plot.Add.VerticalLine(x);
+                vl.Color = color;
+                vl.LinePattern = LinePattern.Dashed;
+            }
+        }
+        else
+        {
+            
+            double x1 = -10000;
+            double y1 = (-line.A * x1 - line.C) / line.B;
+            double x2 = 10000;
+            double y2 = (-line.A * x2 - line.C) / line.B;
+            
+            var l = _plot.Plot.Add.Line(x1, y1, x2, y2);
+            l.Color = color;
+            l.LinePattern = LinePattern.Dashed;
+        }
     }
 }
